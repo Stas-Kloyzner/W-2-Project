936b4eef1f259287b89cd2c96fe3026b67afdbc5
stas.kloy <stas.kloy93@gmail.com>
2022-05-26 18:27:39 +0300

    minor tweaks.

--- password-validator.sh
+++ password-validator.sh
@@ -2,9 +2,10 @@
 # Author: Stanislav Kloyzner
 # 
 # Checks if a given password is longer than set length and contains numbers and alphabet characters both lower case and capital.
+# use example: ./password-validator.sh "MyP@ssw0rd!"
 
 
-#######################################
+#======================================
 # Checks if passed argument is a number between 0-9.
 # Globals:
 #   None
@@ -13,7 +14,7 @@
 # Outputs:
 #   1 if given argument is a number between 0-9.
 #   0 if given argument is not a number between 0-9.
-#######################################
+#======================================
 function isNum {
     if [[ $1 =~ ^[0-9]+$ ]]; then
         echo 1
@@ -22,16 +23,16 @@ function isNum {
     fi
 }
 
-#######################################
-# Checks if passed argument is a lower case alphabet chaacter between a-z.
+#======================================
+# Checks if passed argument is a lower case alphabet character between a-z.
 # Globals:
 #   None
 # Arguments:
 #   A char to check
 # Outputs:
-#   1 if given argument is a lower case alphabet chaacter between a-z.
-#   0 if given argument is not a lower case alphabet chaacter between a-z.
-#######################################
+#   1 if given argument is a lower case alphabet character between a-z.
+#   0 if given argument is not a lower case alphabet character between a-z.
+#======================================
 function isLowCase {
     if [[ $1 =~ ^[a-z]+$ ]]; then
         echo 1
@@ -40,16 +41,16 @@ function isLowCase {
     fi
 }
 
-#######################################
-# Checks if passed argument is a capital case alphabet chaacter between A-Z.
+#======================================
+# Checks if passed argument is a capital case alphabet character between A-Z.
 # Globals:
 #   None
 # Arguments:
 #   A char to check
 # Outputs:
-#   1 if given argument is a capital case alphabet chaacter between A-Z.
-#   0 if given argument is not a capital case alphabet chaacter between A-Z.
-#######################################
+#   1 if given argument is a capital case alphabet character between A-Z.
+#   0 if given argument is not a capital case alphabet character between A-Z.
+#======================================
 function isUpCase {
     if [[ $1 =~ ^[A-Z]+$ ]]; then
         echo 1
@@ -58,7 +59,7 @@ function isUpCase {
     fi
 }
 
-#######################################
+#======================================
 # Iterates on the characters of a passed argument( a password) and checks if it answers all required conditions:
 # length, contains numbers, contains lower and capital alphabet characters.
 # Globals:
@@ -75,12 +76,9 @@ function isUpCase {
 #   A password to check
 # Outputs:
 #   If validation passed: A message telling the validation was passed (colored Green)
-#   If validation failed: A message telling the validation failed and listing the reasons of failur (colored Red)
-# RETURN:
-#   0 If the password passed validation (answered all conditions).
-#   1 If the password failed validation (didn't answer atleast 1 condition).
-#######################################
-function checkPass {    
+#   If validation failed: A message telling the validation failed and listing the reasons of failure (colored Red)
+#======================================
+function validate {    
 
     GREEN='\033[0;32m' 
     RED='\033[0;31m' 
@@ -91,42 +89,58 @@ function checkPass {
     U_CASE=0
     PASS=$1 
 
-    if [ ${#PASS} -ge $LEN ]; then 
-        for (( i=0; i<=${#PASS}; i++ )); do 
-            if [[ $i -lt ${#PASS} && $(($NUM+$L_CASE+$U_CASE)) -lt 3 ]]; then 
-                CHAR=${PASS:$i:1} 
-                if [ $NUM -eq 0 ]; then
-                        NUM=$(isNum "$CHAR")
-                fi        
-                if [[ $L_CASE -eq 0 ]]; then
-                        L_CASE=$(isLowCase "$CHAR")
-                fi        
-                if [[ $U_CASE -eq 0 ]]; then
-                        U_CASE=$(isUpCase "$CHAR")
-                fi
-            elif [[ $i -eq ${#PASS} && $(($NUM+$L_CASE+$U_CASE)) -lt 3 ]]; then # Last iteration after all chars were checked to see which conditions weren't answered.
-                echo -e "${RED}Password ${NC}"$PASS" ${RED}FAILED validation:${NC}"
-                if [[ $NUM -eq 0 ]]; then
-                        echo -e "${RED}  -Password must contain a NUMBER${NC}"
-                fi        
-                if [[ $L_CASE -eq 0 ]]; then
-                        echo -e "${RED}  -Password must contain a LOWER case character${NC}"
-                fi        
-                if [[ $U_CASE -eq 0 ]]; then
-                        echo -e "${RED}  -Password must contain a CAPITAL case character${NC}"
-                fi 
-                exit 1
-            else 
-                i=${#PASS} # Stops the iteration in case all conditions were already answered before last iteration.
-                echo -e "${GREEN}Password${NC} "$PASS" ${GREEN}PASSED validation${NC}"
-                exit 0
+    for (( i=0; i<=${#PASS}; i++ )); do 
+        if [[ $i -lt ${#PASS} && $(($NUM+$L_CASE+$U_CASE)) -lt 3 ]]; then 
+            CHAR=${PASS:$i:1} 
+            if [ $NUM -eq 0 ]; then
+                    NUM=$(isNum "$CHAR")
+            fi        
+            if [[ $L_CASE -eq 0 ]]; then
+                    L_CASE=$(isLowCase "$CHAR")
+            fi        
+            if [[ $U_CASE -eq 0 ]]; then
+                    U_CASE=$(isUpCase "$CHAR")
             fi
-        done
-    else   
-        echo -e "${RED}Password${NC} "$PASS" ${RED}FAILED validation:${NC}"
-        echo -e "  ${RED}-Password must be 10 or more characters long${NC}"
+        elif [[ $(($NUM+$L_CASE+$U_CASE)) -eq 3 && ${#PASS} -ge LEN ]]; then # Last iteration after all chars were checked to see which conditions weren't answered.
+            i=${#PASS} # Stops the iteration in case all conditions were answered.
+            echo -e "${GREEN}Password PASSED validation${NC}"
+            exit 0
+        else 
+            echo -e "${RED}Password FAILED validation:${NC}"
+            if [[ ${#PASS} -le LEN ]]; then
+                    echo -e "${RED}  -Password must be $LEN or more characters long${NC}"
+            fi  
+            if [[ $NUM -eq 0 ]]; then
+                    echo -e "${RED}  -Password must contain a NUMBER${NC}"
+            fi        
+            if [[ $L_CASE -eq 0 ]]; then
+                    echo -e "${RED}  -Password must contain a LOWER case character${NC}"
+            fi        
+            if [[ $U_CASE -eq 0 ]]; then
+                    echo -e "${RED}  -Password must contain a CAPITAL case character${NC}"
+            fi 
+            exit 1
+        fi
+    done
+}
+
+#======================================
+# Checks if a first argument (password) was passed.
+# If the first argument was a password ,try to validate it.
+# If the first argument wasnt passed , print a message (colored red) saying the password wasnt provided.
+# Arguments:
+#   A password to validate.
+# Outputs:
+#   If input argument was incorrect, prints message (colord red) specifying the problem.
+#======================================
+function startValidate {
+
+    if [ -z $1 ]; then 
+        echo -e "\033[0;31mYou didnt pass a password to validate.\033[0m"
         exit 1
+    else
+        validate $1 
     fi
 }
 
-checkPass "$1"
+startValidate $1
